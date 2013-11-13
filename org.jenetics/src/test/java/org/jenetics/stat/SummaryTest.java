75777dfbc3675b1e7eaabf36e336690d9cf1e7bd
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2013-11-14 00:18:01 +0100

    Implement 'CollectibleSummary'.
    --HG--
    branch : org.jenetics-jdk8

--- org.jenetics/src/test/java/org/jenetics/stat/SummaryTest.java
+++ org.jenetics/src/test/java/org/jenetics/stat/SummaryTest.java
@@ -68,6 +68,8 @@ public class SummaryTest {
 		Assert.assertEquals(psummary.getSum(), expected.getSum(), 0.00001);
 		Assert.assertEquals(psummary.getMean(), expected.getMean(), 0.00001);
 		Assert.assertEquals(psummary.getVariance(), expected.getVariance(), 0.00001);
+		Assert.assertEquals(psummary.getSkewness(), expected.getSkewness(), 0.00001);
+		Assert.assertEquals(psummary.getKurtosis(), expected.getKurtosis(), 0.00001);
 	}
 
 }
