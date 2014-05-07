e87aa0380912d165c8882f8f4b2780ee7db46f44
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2014-05-07 22:27:50 +0200

    Fix javadoc.
    --HG--
    branch : r3.0.0

--- org.jenetics/src/main/java/org/jenetics/PopulationSummaryStatistics.java
+++ org.jenetics/src/main/java/org/jenetics/PopulationSummaryStatistics.java
@@ -22,8 +22,8 @@ package org.jenetics;
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz
  *         Wilhelmstötter</a>
- * @version @__version__@ &mdash; <em>$Date$</em>
- * @since @__version__@
+ * @version 3.0 &mdash; <em>$Date$</em>
+ * @since 3.0
  */
 public class PopulationSummaryStatistics {
 }
