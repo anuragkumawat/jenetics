52cfee6282a1ee94f12a12eafc62aa1de43a357a
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2014-07-14 19:52:38 +0200

    Update version string.
    --HG--
    branch : r3.0.0

--- org.jenetics/src/jmh/java/org/jenetics/util/RandomEnginePerf.java
+++ org.jenetics/src/jmh/java/org/jenetics/util/RandomEnginePerf.java
@@ -36,8 +36,8 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
- * @version !__version__! &mdash; <em>$Date$</em>
- * @since !__version__!
+ * @version 3.0 &mdash; <em>$Date$</em>
+ * @since 3.0
  */
 public class RandomEnginePerf {
 
