13482b9b83afb5a6100e2bd332211708edc09cfb
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2014-09-15 23:12:15 +0200

    Some engine implementations.
    --HG--
    branch : r3.0.0

--- org.jenetics/src/main/java/org/jenetics/engine/PopulationSummary.java
+++ org.jenetics/src/main/java/org/jenetics/engine/PopulationSummary.java
@@ -43,7 +43,7 @@ import org.jenetics.stat.IntSummary;
  * @since 3.0
  * @version 3.0 &mdash; <em>$Date$</em>
  */
-final class PopulationSummary<
+public final class PopulationSummary<
 	G extends Gene<?, G>,
 	C extends Comparable<? super C>
 >
