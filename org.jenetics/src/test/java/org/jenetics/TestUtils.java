0d16eb6ab875766dfa0aa1f7a375b46db2b0edd3
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2014-09-17 20:06:38 +0200

    Remove 'GeneticAlgorithm' class. Has been removed by the 'Engine'.
    --HG--
    branch : r3.0.0

--- org.jenetics/src/test/java/org/jenetics/TestUtils.java
+++ org.jenetics/src/test/java/org/jenetics/TestUtils.java
@@ -201,12 +201,6 @@ class TestUtils {
 	public static final Function<Genotype<DoubleGene>, Double> FF =
 		gt -> gt.getGene().getAllele();
 
-	public static GeneticAlgorithm<DoubleGene, Double> GA() {
-		return new GeneticAlgorithm<>(
-				Genotype.of(DoubleChromosome.of(0, 1)), FF
-			);
-	}
-
 
 	public static Phenotype<DoubleGene, Double> newDoublePhenotype(final double value) {
 		return Phenotype.of(Genotype.of(
