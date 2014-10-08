44690abe0f777b40a8aacd6069d25184246e362d
Franz Wilhelmstötter <franz.wilhelmstoetter@gmx.at>
2014-10-08 22:54:40 +0200

    Update class version number.
    --HG--
    branch : r3.0.0

--- org.jenetics/src/main/java/org/jenetics/util/Factory.java
+++ org.jenetics/src/main/java/org/jenetics/util/Factory.java
@@ -26,7 +26,7 @@ import java.util.stream.Stream;
  *
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since 1.0
- * @version 1.0 &mdash; <em>$Date$</em>
+ * @version 3.0 &mdash; <em>$Date$</em>
  */
 @FunctionalInterface
 public interface Factory<T> {
